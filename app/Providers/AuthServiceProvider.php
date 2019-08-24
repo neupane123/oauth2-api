3f70b6df4f12753393012946a489c55836481cdd
Aayan Neupane <aayanneupane25@gmail.com>
2019-08-24 17:48:16 +0545

    auth scafold added

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,8 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        
-        Passport::routes;
+
+        Passport::routes();
 
         //
     }
